6688fd518719a9fe1ef225efb100b221352155d5
Rimantas <belauzas@gmail.com>
2023-11-24 11:17:12 +0200

    Done

---
+++ src/components/country/Country.jsx
@@ -0,0 +1,11 @@
+import style from './Country.module.css';
+
+export function Country({ data }) {
+    console.log(data);
+    return (
+        <div className={style.country}>
+            <img className={style.flag} src={data.flags.png} alt={data.name.official} />
+            <div className={style.countryName}>{data.name.common}</div>
+        </div>
+    );
+}
