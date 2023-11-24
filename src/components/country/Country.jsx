5fa1c8cb5d6baa769c53318ab448a5029112f769
Rimantas <belauzas@gmail.com>
2023-11-24 11:17:28 +0200

    Fix

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,7 +1,6 @@
 import style from './Country.module.css';
 
 export function Country({ data }) {
-    console.log(data);
     return (
         <div className={style.country}>
             <img className={style.flag} src={data.flags.png} alt={data.name.official} />
